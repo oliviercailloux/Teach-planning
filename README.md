81864bbd04a34394fcdc8071ae635c6ec243c552
amelieq <amelie.quach@dauphine.eu>
2016-11-23 20:22:12 +0100

    RAZ du squelette du projet

--- README.md
+++
@@ -1,4 +0,0 @@
-# teach_planning
-# Ceci est un test Commit Sandratra
-# Ceci est un test Commit Amélie
-# Ceci est un test RECUP puis Commit Cathy
--- src/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package src;
-
-public class HelloWorld {
-	
-	public static void main(String args[]) {
-		System.out.println("Hello people, Skeleton Test ! ");
-	}
-}
