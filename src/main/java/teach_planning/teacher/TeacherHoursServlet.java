8c09b054541129a0dca9e48aed7c4adf8409ed53
amelieq <amelie.quach@dauphine.eu>
2017-01-25 01:25:57 +0100

    Début de l'issue 9 : - ajout de la vue (jsps/nbHours.jsp) - début du servlet (TeacherHoursServlet.java) - modification des vues (login.jsp) et (index.jsp)

---
+++ src/main/java/teach_planning/teacher/TeacherHoursServlet.java
@@ -0,0 +1,43 @@
+package teach_planning.teacher;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Servlet implementation class TeacherHoursServlet
+ */
+
+@WebServlet(name="TeacherHoursServlet", urlPatterns={"/teacherHours"})
+public class TeacherHoursServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+      
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		HttpSession session = req.getSession();
+		
+		resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
+		resp.setContentType(MediaType.TEXT_HTML);
+		resp.setLocale(Locale.FRENCH);
+		
+		if(session.getAttribute("typeSession").equals("admin")){
+			int nbHours = Integer.parseInt(req.getParameter("nbHours"));
+			String firstName = req.getParameter("teacherFirstname");
+			String lastName = req.getParameter("teacherLastname");
+		}else if(session.getAttribute("typeSession").equals("teacher")){
+			int nbHours = Integer.parseInt(req.getParameter("nbHours"));
+		}
+	}
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -7,27 +7,15 @@
 		<%
 			if(session.getAttribute("typeSession") != null) {
 		%>
-
-			<form method="GET" name="listandsearch" action="listandsearch">
-				<table>
-					<tr>
-						<td><input type="submit" value="Rechercher les informations sur un enseignement" /></td>
-					</tr>
-				</table>
-			</form>
-			
-			<form method="GET" name="listandsearch" action="chargeTeacher">
-				<table>
-					<tr>
-						<td><input type="submit" value="Rechercher les informations sur un enseignant" /></td>
-					</tr>
-				</table>
-			</form>
-			
-			<a href="jsps/prefView/prefForm.jsp">Ajouter les préférences d'un enseignant</a><br/>
-			
-			<a href="jsps/serviceFile.jsp">Imprimer la fiche de service d'un enseignant</a>
-	
+			<ul>
+				<li><a href="jsps/addTeacher.jsp">Ajouter un nouvel enseignant</a></li>
+				<li><a href="jsps/addTeaching.jsp">Ajouter un nouvel enseignement</a></li>
+				<li><a href="jsps/prefView/prefForm.jsp">Ajouter les préférences d'un enseignant</a></li>
+				<li><a href="jsps/serviceFile.jsp">Imprimer la fiche de service d'un enseignant</a></li>
+				<li><a href="chargeTeacher">Rechercher les informations sur un enseignant</a></li>
+				<li><a href="listandsearch">Rechercher les informations sur un enseignement</a></li>
+				<li><a href="teacherHours">Renseigner les heures d'un enseignant</a></li>
+			</ul>
 		<%		
 			} else {
 		%>
